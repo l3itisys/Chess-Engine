10e2b83b32eabbfc63230544d549be4f009525c2
l3itisys <leiticia.tlb@gmail.com>
2023-01-20 12:06:19 -0500

    Chess Engine with C

--- defs.h
+++ defs.h
@@ -9,7 +9,7 @@ typedef unsigned long long U64; /* Unsigned 64 bits board */
 #define MAXGAMEMOVES 2048 
 
 enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK }; 
-enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_G, FILE_H, FILE_NONE }; 
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE }; 
 enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
 
 enum { WHITE, BLACK, BOTH };
