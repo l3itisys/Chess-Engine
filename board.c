5db4201a4ef6a8574727c1286650694918e3d003
l3itisys <leiticia.tlb@gmail.com>
2023-01-28 19:05:50 -0500

    Chess Engine with C

---
+++ board.c
@@ -0,0 +1,10 @@
+// board.c
+
+#include "stdio.h"
+#include "defs.h"
+
+void ResetBoard(S_BOARD *pos) {
+
+  int index = 0;
+
+} 
--- defs.h
+++ defs.h
@@ -106,6 +106,9 @@ extern int Sq120ToSq64[BRD_SQ_NUM]; // Return the equivalent index from 64 bit B
 extern int Sq64ToSq120[64];
 extern U64 SetMask[64]; 
 extern U64 ClearMask[64]; 
+extern U64 PieceKeys[13][120];
+extern U64 SideKey;
+extern U64 CastleKeys[16]; 
 
 /* FUNCTIONS */
 // init.c 
@@ -115,4 +118,8 @@ extern void AllInit ();
 extern void PrintBitBoard(U64 bb);
 extern int PopBit(U64 *bb); 
 extern int CountBits(U64 b); 
+
+// hashkeys.c
+extern U64 GeneratePosKey(const S_BOARD *pos); 
+
 #endif 
